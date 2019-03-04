943612929b164e9149f0f7c01eaedd9b9750f6e6
Hamza Riaz <hrsheikh05@gmail.com>
2019-03-04 20:48:35 +0000

    Second Version

---
+++ src/main/java/com/dami/dailylights/Login.java
@@ -0,0 +1,42 @@
+package com.dami.dailylights;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    Button b1;
+    EditText username;
+    EditText password;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = (EditText) findViewById(R.id.Username);
+        password = (EditText) findViewById(R.id.Password);
+        b1 = (Button) findViewById(R.id.LoginButton);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Authentication(username.getText().toString(), password.getText().toString());
+            }
+        });
+
+    }
+
+    //Login Screen Authentication
+    private void Authentication(String UserName, String PassWord){
+        if((UserName.equals("Admin")) && (PassWord.equals("Hamza123"))){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            Login.this.startActivity(intent);
+        }
+        else{
+            Toast.makeText(getBaseContext(), "Enter Password Again!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
